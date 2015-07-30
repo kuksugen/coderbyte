f8d02da86e4274c01c5ee9c4ef498748acd2eae8
kuksugen <kuksugen@kaist.ac.kr>
2015-07-30 21:14:34 +0900

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,10 @@
+def LetterChanges(str)
+
+  # code goes here
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)  
