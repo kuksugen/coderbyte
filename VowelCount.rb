926df07361acb47d7a59b5ef8c2bc1755b710f63
kuksugen <kuksugen@kaist.ac.kr>
2015-07-31 13:40:45 +0900

    Create VowelCount.rb

---
+++ VowelCount.rb
@@ -0,0 +1,20 @@
+def VowelCount(str)
+
+  # code goes here
+  arr = str.chars.to_a
+  len = str.length
+  result = 0
+  0.upto(len-1) do |y|
+    if (arr[y] == 'a' || arr[y] == 'e' || arr[y]=='i' || arr[y] =='o' || arr[y] =='u') 
+      result = result +1
+   
+    end
+  end
+  return result
+      
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+VowelCount(STDIN.gets)  
